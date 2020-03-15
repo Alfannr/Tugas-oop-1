6db7cfbaff3e25c242a14c7a956882243816fac7
Alfannr <57250800+Alfannr@users.noreply.github.com>
2020-03-15 20:30:54 +0700

    Add files via upload

---
+++ Mahasiwa.java
@@ -0,0 +1,23 @@
+package Tugas;
+
+
+ public class Mahasiswa {
+    String nim, nama, kelas;
+    public static void main(String[] args) {
+        
+       
+        
+        Mahasiswa x = new Mahasiswa();
+        
+        x.nim = "18090139";
+        x.nama = "Alfan Nur Rabbani";
+        x.kelas = "4D";
+        
+        System.out.println("NIM : " + x.nim);
+        System.out.println("Nama : " + x.nama);
+        System.out.println("Kelas : " + x.kelas);
+    }
+    
+}
+
+
